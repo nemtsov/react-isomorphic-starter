c3f6febb3b8f339dcc6e728acb18bc03f240bd0e
Yuriy Nemtsov <nemtsov@gmail.com>
2015-02-21 16:13:18 -0500

    gulp/sass: nodemon

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  livereload = require('gulp-livereload');
 
 exports.toCss = sassToCss;
 exports.toCssWatch = sassToCssWatch;
@@ -9,10 +10,12 @@ function sassToCss() {
   return gulp.src('./lib/components/App/App.scss')
     .pipe(sass())
     .on('error', gutil.log.bind(gutil, 'SASS Error'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload());
 }
 
 function sassToCssWatch() {
+  livereload.listen();
   sassToCss();
   return gulp.watch('./lib/components/**/*.scss', sassToCss);
 }
